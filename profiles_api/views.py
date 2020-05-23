6253c9c84e974d47cc65893740ca49f0cda46a56
Mohd saddam husain <msaddam2786@gmail.com>
2020-05-23 14:26:23 +0530

    Added POST, PUT, PATCH and DELETE method to our ApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 # Create your views here.
 
 
 class HelloApiView(APIView):
     """Test APIView"""
+    serializer_class = serializers.HelloSerializers
 
     def get(self,request,format=None):
         """Return a list of APIView feature"""
@@ -16,3 +20,37 @@ class HelloApiView(APIView):
          'Is mapped manually to URLs'
         ]
         return Response({'message':'Hello','an_apiview':an_apiview})
+    def post(self,request):
+        """Create a hello message with our name"""
+        serializer  = self.serializer_class(data=request.data)
+        # print(request.data)
+        # serializer=request.data
+        ###Start---------------------
+        """ At this point we've translated the model instance into Python native datatypes.
+        To finalise the serialization process we render the data into json."""
+        # from rest_framework.renderers import JSONRenderer
+        #
+        # json = JSONRenderer().render(request.data)
+        # print(json)
+        # return Response({'msg':'Hi'})
+        ###-----------------END
+        if serializer.is_valid():
+
+            name = serializer.validated_data.get('name')
+            message  =f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request,pk=None):
+        """Handle the updating an object"""
+        return Response({"method":"put"})
+
+    def patch(self,request,pk=None):
+        """Handle the partial update of an object"""
+        return Response({"method":"patch"})
+
+    def delete(self,request,pk=None):
+        """Delete an object"""
+        return Response({"method":"delete"})
+     
